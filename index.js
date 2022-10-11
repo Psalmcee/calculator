7c60da571e1f2f3989d31c2751c672701376d8fc
Psalmcee <100202765+Psalmcee@users.noreply.github.com>
2022-10-11 22:56:36 +0100

    Added index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const display = document.getElementById('display');
+const zero = document.querySelector('zero');
+const one = document.querySelector('one');
+const two = document.querySelector('two');
+const three = document.querySelector('three');
+const four = document.querySelector('four');
+const five = document.querySelector('five');
+const six = document.querySelector('six');
+const seven = document.querySelector('seven');
+const eight = document.querySelector('eight');
+const nine = document.querySelector('nine');
+const add = document.querySelector('plus');
+const subtract = document.querySelector('minus');
+const multiply = document.querySelector('multiply');
+const divide = document.querySelector('divide');
+const result = document.querySelector('result');
+
+
+one.addEventListener('click', () => {
+    alert("button is clicked")
+})
